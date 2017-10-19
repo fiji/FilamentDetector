f21f7748b800571d05393a4f8d37d5645ab2d930
Hadrien Mary <hadrien.mary@gmail.com>
2017-10-18 22:16:33 -0400

    Autoformat

--- src/main/java/fiji/plugin/filamentdetector/gui/controller/AboutController.java
+++ src/main/java/fiji/plugin/filamentdetector/gui/controller/AboutController.java
@@ -27,8 +27,13 @@ public class AboutController extends Controller implements Initializable {
 
 	private void loadAboutText() {
 		String text = "<h1><a></a>About FilamentDetector</h1>\n"
-				+ "<p>The <strong>detection</strong> step is based on the ImageJ1 implementation of Steger, 1998 (<a href=\"http://imagej.net/Ridge_Detection\">http://imagej.net/Ridge_Detection</a>). The <strong>tracking</strong> step is based on the Jaqaman tracking algorithm (Jaqaman, 2008) implemented in <a href=\"https://imagej.net/TrackMate\">TrackMate</a>. Instead of using the euclidean distance as a cost function, <strong>FilamentDetector</strong> usez the bounding boxe of the detected filaments to compute the <a href=\"https://en.wikipedia.org/wiki/Jaccard_index\">Intersection over Union</a>.</p>\n"
-				+ "<ul>\n"
+				+ "<p>The <strong>detection</strong> step is based on the ImageJ1 implementation of Steger,"
+				+ " 1998 (<a href=\"http://imagej.net/Ridge_Detection\">http://imagej.net/Ridge_Detection</a>)."
+				+ " The <strong>tracking</strong> step is based on the Jaqaman tracking algorithm (Jaqaman, 2008)"
+				+ " implemented in <a href=\"https://imagej.net/TrackMate\">TrackMate</a>. Instead of using the "
+				+ "euclidean distance as a cost function, <strong>FilamentDetector</strong> usez the bounding boxe "
+				+ "of the detected filaments to compute the <a href=\"https://en.wikipedia.org/wiki/Jaccard_index\">"
+				+ "Intersection over Union</a>.</p>\n" + "<ul>\n"
 				+ "<li>Source code: <a href=\"https://github.com/hadim/FilamentDetector\">https://github.com/hadim/FilamentDetector</a></li>\n"
 				+ "<li>Report an issue: <a href=\"https://github.com/hadim/FilamentDetector/issues\">https://github.com/hadim/FilamentDetector/issues</a></li>\n"
 				+ "<li>Wiki: <a href=\"http://imagej.net/FilamentDetector\">http://imagej.net/FilamentDetector</a></li>\n"
