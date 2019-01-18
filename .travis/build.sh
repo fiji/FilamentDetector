c3417abef7042b05fd7de05a7bc532b4e09dee7d
Hadrien Mary <hadrien.mary@gmail.com>
2019-01-18 16:36:20 -0500

    fix upload script

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
 sh travis-build.sh
-curl -fsLO https://raw.githubusercontent.com/fiji/fiji/update-site/bin/upload-update-site.sh
+curl -fsLO https://raw.githubusercontent.com/fiji/fiji/master/bin/upload-update-site.sh
 sh upload-update-site.sh
