0e7be6d2147cdbb0f198043af95048e7cdab0d81
Curtis Rueden <ctrueden@wisc.edu>
2019-05-17 16:53:41 -0500

    Travis: prevent false positives
    When the travis-build.sh step fails, the build wasn't failing.

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
 sh travis-build.sh
 curl -fsLO https://raw.githubusercontent.com/fiji/fiji/master/bin/upload-update-site.sh
